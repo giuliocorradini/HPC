4a26ccb3c59644b41a96524e5da7429ed4ab3119
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-11-13 14:09:51 +0100

    Connection scripts

---
+++ scripts/connect.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ssh -i ./keys/id_rsa_nano_unimore -p 22709 '299721@localhost'
+
---
+++ scripts/tunnel.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+ssh -N -L 22709:elgamal.mat.unimore.it:22709 student@wks-marongiu2.mat.unimo.it -v
